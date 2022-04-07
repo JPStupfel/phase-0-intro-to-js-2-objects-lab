21156d654bd382e840872fd168a9ebad8b166859
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-07 13:00:02 -0500

    commit tree 56b58c8bbae2a91de7c43a9562fc365f7e2bc99a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {name:'',street:''}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    let newObj = {...obj};
+    newObj[key] = value
+    return newObj
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+
+function deleteFromEmployeeByKey(obj, key){
+   let newObj = {...obj}
+   delete newObj[key]
+   return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+delete obj[key]
+return obj
+}
